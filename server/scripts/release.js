59a24cd19a04d5abf616f95291015a54bbbd66de
devclassic <devclassic@hotmail.com>
2026-07-08 14:17:27 +0800

    重构生成脚本

--- server/scripts/release.js
+++ server/scripts/release.js
@@ -5,10 +5,7 @@ rimraf.sync('dist')
 rimraf.sync('release')
 
 spawn.sync('nest', ['build'], { stdio: 'inherit' })
-spawn.sync(
-  'deno',
-  'compile -A --node-modules-dir --unstable-detect-cjs --target x86_64-unknown-linux-gnu -o release/server dist/main.js'.split(
-    ' ',
-  ),
-  { stdio: 'inherit' },
-)
+
+const compileArgs =
+  'compile -A --node-modules-dir --unstable-detect-cjs --target x86_64-unknown-linux-gnu -o release/server dist/main.js'
+spawn.sync('deno', compileArgs.split(' '), { stdio: 'inherit' })
