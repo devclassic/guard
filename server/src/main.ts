23651c2642531bbfe396e5404bd4b15604af8c21
devclassic <devclassic@hotmail.com>
2026-07-14 11:33:06 +0800

    提交

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,8 @@ import { Logger } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.enableCors()
+  app.useStaticAssets('public/client', { prefix: '/' })
+  app.useStaticAssets('public/admin', { prefix: '/admin' })
   const port = process.env.PORT ?? 3000
   await app.listen(port)
   Logger.log(`http://localhost:${port}`, 'Server')
