34c957869bbfbd4f4215dcffb5db0abe520a8ccc
devclassic <devclassic@hotmail.com>
2026-07-16 17:22:25 +0800

    提交

--- client-ui/vite.config.js
+++ client-ui/vite.config.js
@@ -6,5 +6,24 @@ export default defineConfig({
   base: './',
   build: {
     outDir: 'dist/client',
+    chunkSizeWarningLimit: 1000,
+    rolldownOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue') || id.includes('pinia')) {
+              return 'vue'
+            }
+            if (id.includes('vant')) {
+              return 'vant'
+            }
+            if (id.includes('axios')) {
+              return 'axios'
+            }
+            return 'vendor'
+          }
+        },
+      },
+    },
   },
 })
